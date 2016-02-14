a4f88229abac0838a8a6f730545c271dfc727460
byanofsky <brandon@mywpexpert.com>
2016-02-14 11:18:15 -0800

    Added connection with API and refine design

--- footer.php
+++ footer.php
@@ -8,13 +8,24 @@
     <script src="bower_components/bootstrap/assets/js/ie10-viewport-bug-workaround.js"></script>
 
     <script>
+    function showForm() {
+      $( ".create-text-field" ).removeClass( "hidden" );
+    }
+    function hideForm() {
+      $( ".create-text-field" ).addClass( "hidden" );
+    }
     function appendText() {
-        var txtfield = '<div class="text-field">Sample Text</div>';              // Create text with HTML
-        $("#image-overlay").append(txtfield);     // Append new elements
-        $(function() {
-          $( ".text-field" ).draggable({ containment: "parent" });
-          $( ".text-field" ).resizable();
-        });
+        var fieldName = document.getElementById("field-name").value;
+        if (fieldName.length > 0) {
+          var txtfield = '<div class="text-field">' + fieldName + '</div>';              // Create text with HTML
+          $("#image-overlay").append(txtfield);     // Append new elements
+          $(function() {
+            $( ".text-field" ).draggable({ containment: "parent" });
+            $( ".text-field" ).resizable();
+          });
+        }
+        hideForm();
+        $('#field-name').val('');
     }
     </script>
 
@@ -35,7 +46,45 @@
 
   $('.cloudinary_fileupload').unsigned_cloudinary_upload("hevbb0vi", 
   { cloud_name: 'restpdf' }
-  ).bind('cloudinarydone', function(e, data) {
+  ).bind('cloudinaryprogress', function(e, data) { 
+
+  // var progressbar = $( "#progressbar" ),
+  //     progressLabel = $( ".progress-label" );
+ 
+  //   progressbar.progressbar({
+  //     value: false,
+  //     change: function() {
+  //       progressLabel.text( progressbar.progressbar( "value" ) + "%" );
+  //     },
+  //     complete: function() {
+  //       progressLabel.text( "Complete!" );
+  //     }
+  //   });
+ 
+  //   function progress() {
+  //     var val = progressbar.progressbar( "value" ) || 0;
+ 
+  //     progressbar.progressbar( "value", val + 2 );
+ 
+  //     if ( val < 99 ) {
+  //       setTimeout( progress, 80 );
+  //     }
+  //   }
+ 
+  //   setTimeout( progress, 2000 );
+
+   $('.progress_bar').css('width', 
+    Math.round((data.loaded * 100.0) / data.total) + '%');
+
+   setTimeout( function() {
+          $('.progress_bar').css('width', 
+          Math.round((data.loaded * 100.0) / data.total) + '%');
+    }, 500 );
+
+  }).bind('cloudinarydone', function(e, data) {
+    setTimeout(function() {$('.load-successful').append('<p class="loading">Upload Completed.</p>');}, 200);
+    $('.progress_bar').addClass('hidden')
+    setTimeout(function() {$('.load-successful').append('<p class="loading">Creating Preview.</p>');}, 1000);
 
     pdfURL = $.cloudinary.image(data.result.public_id, 
       { format: 'jpg', cloud_name: 'restpdf' } );
@@ -50,9 +99,11 @@
           imgWidth = $( '.image-display img' ).width();
           console.log(imgWidth);
           console.log(imgHeight);
+          $('.loading').remove();
           $('.upload_form').addClass('hidden');
-          $('#add-text').removeClass('hidden');
-          $('#submit-pdf').removeClass('hidden');
+          $('.markup-buttons').removeClass('hidden');
+          $('#stepone').addClass('hidden');
+          $('#steptwo').removeClass('hidden');
 
           $( "#image-overlay").height(imgHeight).width(imgWidth);
           clearInterval(checkExist);
@@ -61,27 +112,21 @@
        }
     }, 100); // check every 100ms
 
-  }).bind('cloudinaryprogress', function(e, data) { 
-
-    $('.progress_bar').css('width', 
-      Math.round((data.loaded * 100.0) / data.total) + '%'); 
-
   });
 
   $('#submit-pdf').click(function() {
     markedPDF = {
-      url: pdfURL[0].currentSrc,
-      size: {
-        width: imgWidth,
-        height: imgHeight
+      pdf: {
+        url: pdfURL[0].currentSrc,
+        height: imgHeight,
+        width: imgWidth
       },
-      fields: new Object()
+      fields: new Array()
       }
 
     var textBoxes = $('.text-field');
     for (i = 0; i < textBoxes.length; i++) {
-        markedPDF['fields']['field' + i] = new Object();
-        markedPDF['fields']['field' + i]['position'] = new Object();
+        var fieldName = $(textBoxes[i]).text();
         if ($(textBoxes[i]).css("left") == 'auto') {
           var textBoxLeft = 0;
         } else {
@@ -92,11 +137,33 @@
         } else {
           var textBoxTop = $(textBoxes[i]).css("top").substring(0, $(textBoxes[i]).css("top").length - 2);
         }
-        console.log($(textBoxes[i]).css("left"));
-        markedPDF['fields']['field' + i]['position']['left'] = Number(textBoxLeft)/imgWidth;
-        markedPDF['fields']['field' + i]['position']['top'] = Number(textBoxTop)/imgHeight;
+        markedPDF['fields'][i] = {
+          name: fieldName,
+          x: Number(textBoxLeft)/imgWidth,
+          y: Number(textBoxTop)/imgHeight,
+          font: "Helvetica",
+          size: 20
+        }
     }
     console.log(markedPDF);
+    $.ajax('http://api.restpdf.io/templates', {
+        data: JSON.stringify(markedPDF),
+        contentType: "application/json",
+        type: "POST"
+      }).then(
+      function(result){
+        console.log(result);
+        $('#steptwo').addClass('hidden');
+        $('.markup-buttons').addClass('hidden');
+        $('.image-display').addClass('hidden');
+        var resultText = '<p class="lead">Your API Endpoint:</p><p>' + result.api_endpoint + '</p><p class="lead">Your Available Fields</p><ul>';
+        for (i = 0; i < markedPDF['fields'].length; i++) {
+          resultText += "<li>" + markedPDF['fields'][i]['name'] + "</li>";
+        }
+        resultText += '</ul>';
+        $('#results').append(resultText);
+      }
+    );
   });
 
 </script>
--- index.php
+++ index.php
@@ -11,7 +11,8 @@
 
           <div class="inner cover">
             <h1 class="cover-heading">RESTpdf</h1>
-            <p class="lead">Welcome to RESTpdf. Let's get started. Either drag your PDF into your browser window, or click the button to select the file from your computer.</p>
+            <p class="lead" id="stepone">Welcome to RESTpdf. Let's get started. Either drag your PDF into your browser window, or click the button to select the file from your computer.</p>
+            <p class="lead hidden" id="steptwo">Your PDF is now ready for you to mark up. Click "Create A New Text Field", give it a name, add it to your PDF, and then position it. When done adding all fields, click submit.</p>
             <!-- <form class="upload_form">
               <input type="file" name="file" class="cloudinary_fileupload" role="button">
             </form> -->
@@ -26,22 +27,36 @@
 
             <div class="upload_field"></div>
 
+            <!-- <div id="progressbar" class="progress_bar"><div class="progress-label">Loading...</div></div> -->
             <div class="progress_bar"></div>
+            <div class="load-successful"></div>
 
-            <button class="btn btn-lg btn-primary hidden" id="add-text" onclick="appendText()">
-              <i class="glyphicon glyphicon-plus"></i>
-              <span>Add Text Field</span>
-            </button>
-            <button class="btn btn-lg btn-success hidden" id="submit-pdf">
-              <i class="glyphicon glyphicon-ok"></i>
-              <span>Submit</span>
-            </button>
+            <div class="markup-buttons hidden">
+              <button class="btn btn-lg btn-primary" id="add-text" onclick="showForm()">
+                <i class="glyphicon glyphicon-plus"></i>
+                <span>Create New Text Field</span>
+              </button>
+              <button class="btn btn-lg btn-success" id="submit-pdf">
+                <i class="glyphicon glyphicon-ok"></i>
+                <span>Submit</span>
+              </button>
+            </div>
+
+            <div class="create-text-field input-group input-group-lg hidden">
+              <input class="form-control" id="field-name" type="text" name="fieldname" placeholder="Input Field Name">
+              <span class="input-group-btn">
+                <button onclick="appendText()" class="btn btn-default" type="button">Add Field to PDF</button>
+              </span>
+            </div>
 
             <div class="image-display">
               <div id="image-overlay">
               </div>
               <!-- REmove before going live -->
               <!-- <img src="http://res.cloudinary.com/restpdf/image/upload/ufwlaqatvv3d6hbsgjl2.jpg"> -->
+            </div>
+
+            <div id="results">
             </div>
 
           </div>
